b32c242b94bb13ee7cba2f332d9f576b9b903f31
ngdangkietse <kietnguyen17052001@gmail.com>
2024-12-28 13:48:11 +0700

    refactor(utils): public func converter

--- util/converter.util.go
+++ util/converter.util.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Convert2UUID converts list of string to list of UUID
-func convert2UUID(ids []string) []uuid.UUID {
+func Convert2UUID(ids []string) []uuid.UUID {
 	distinct := lo.Uniq(ids)
 	return lo.Map(distinct, func(id string, _ int) uuid.UUID {
 		return uuid.MustParse(id)
@@ -14,7 +14,7 @@ func convert2UUID(ids []string) []uuid.UUID {
 }
 
 // Convert2String converts list of UUID to list of string
-func convert2String(ids []uuid.UUID) []string {
+func Convert2String(ids []uuid.UUID) []string {
 	distinct := lo.Uniq(ids)
 	return lo.Map(distinct, func(id uuid.UUID, _ int) string {
 		return id.String()
